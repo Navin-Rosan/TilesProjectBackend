db17486e820a0fbed6b723009b2bd7abe381935a
Navin-Rosan <140579190+Navin-Rosan@users.noreply.github.com>
2025-02-22 15:31:37 +0530

    Update TilesController.java

--- src/main/java/com/TilesDesign/controller/TilesController.java
+++ src/main/java/com/TilesDesign/controller/TilesController.java
@@ -16,7 +16,7 @@ import com.TilesDesign.service.TilesService;
 
 @RestController
 @RequestMapping("/design")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class TilesController {
 	
 	@Autowired
